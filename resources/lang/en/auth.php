4fcf0ae62bc5901d75231d51daaba98fa3dd05c1
apolinar <Apolinarramos469@gmail.com>
2020-04-24 00:47:51 +0800

    Update auth.php

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Check your details again.',
+    'throttle' => 'Too many login attempts. Please try again later',
 
 ];
