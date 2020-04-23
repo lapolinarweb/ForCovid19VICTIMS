007724adbf7544f77362c0f3acecb4e1fecfa4ef
apolinar <Apolinarramos469@gmail.com>
2020-04-24 01:09:32 +0800

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User; POLLYANNA
+use ForCoved19VICTIMS\lapolinar;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -18,9 +18,9 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name, 'PollyAnna'
-        'email' => $faker->unique()->safeEmail, apolinaramos2368@gmail.com
-        'email_verified_at' => now(),
+        'name' => $faker->name, 'FullName'
+        'email' => $faker->unique()->Valid Email
+        'email_verified_at' => now(Check your Email Inbox),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
